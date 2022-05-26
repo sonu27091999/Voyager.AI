bb1511375064f16e9b5f692e9e62bd2c73e02963
Sonu Kumar <sonu.27091999@gmail.com>
2022-05-26 12:04:28 +0530

    Network call using Axios/Fetch

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ListItem from "./ListItems/ListItem";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Products = () => {
     const [items, setItems] = useState([
@@ -17,23 +18,40 @@ const Products = () => {
             price: 460,
             title: "Title of the Item 2",
             thumbnail: 'placeholder.png'
-        } ,
+        },
         {
             id: 2,
             discountedPrice: 350,
             price: 460,
             title: "Title of the Item 3",
             thumbnail: 'placeholder.png'
-        }   
+        }
     ]);
+
+    useEffect(() => {
+        //@@@@@@ using Fetch API
+
+        // const results = fetch('https://hello-world-52dbb-default-rtdb.firebaseio.com/items.json')
+        //     .then(response => response.json())
+        //     .then(data => console.log(data))
+        //     .catch(error => console.log(error));
+
+        //@@@@@@@@ using axioms
+        axios.get('https://hello-world-52dbb-default-rtdb.firebaseio.com/items.json')
+            .then(resopnse => console.log(resopnse))
+            .catch(error => console.log(error));
+
+    }, [])
+
+
     return (
         <div className='product-list'>
             <div className='product-list--wrapper'>
                 {/* <ListItem data={items[0]} />
                 <ListItem data={items[1]} /> */}
-                {/* {[<div>hello div</div>,<p>hello p</p>]} */} 
+                {/* {[<div>hello div</div>,<p>hello p</p>]} */}
                 {
-                    items.map((item)=>{
+                    items.map((item) => {
                         return <ListItem key={item.id} data={item} />
                     })
                 }
