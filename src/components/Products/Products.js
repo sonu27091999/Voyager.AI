d70d4dd6fab08b8f93ca533bf646cdfb870d7f40
Sonu Kumar <sonu.27091999@gmail.com>
2022-05-26 12:22:35 +0530

    HTTP-data-handling

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,39 +1,34 @@
 import React from 'react'
 import ListItem from "./ListItems/ListItem";
 import { useState } from 'react';
+import axios from 'axios'
 
 const Products = () => {
-    const [items, setItems] = useState([
-        {
-            id: 0,
-            discountedPrice: 340,
-            price: 450,
-            title: "Title of the Item 1",
-            thumbnail: 'placeholder.png'
-        },
-        {
-            id: 1,
-            discountedPrice: 350,
-            price: 460,
-            title: "Title of the Item 2",
-            thumbnail: 'placeholder.png'
-        } ,
-        {
-            id: 2,
-            discountedPrice: 350,
-            price: 460,
-            title: "Title of the Item 3",
-            thumbnail: 'placeholder.png'
-        }   
-    ]);
+    const [items, setItems] = useState([]);
+
+    //@@@@@@@@ using axioms
+    axios.get('https://hello-world-52dbb-default-rtdb.firebaseio.com/items.json')
+        .then(resopnse => {
+            const data = resopnse.data;
+            const transformedData = data.map((item, index) => {
+                return {
+                    ...item,
+                    id: index
+                }
+            })
+            console.log(transformedData);
+            setItems(transformedData);
+        })
+        .catch(error => console.log(error));
+
     return (
         <div className='product-list'>
             <div className='product-list--wrapper'>
                 {/* <ListItem data={items[0]} />
                 <ListItem data={items[1]} /> */}
-                {/* {[<div>hello div</div>,<p>hello p</p>]} */} 
+                {/* {[<div>hello div</div>,<p>hello p</p>]} */}
                 {
-                    items.map((item)=>{
+                    items.map((item) => {
                         return <ListItem key={item.id} data={item} />
                     })
                 }
