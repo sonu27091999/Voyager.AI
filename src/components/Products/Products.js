2a80b2150da2d747308a7fc2e4d20a3e64cede1b
Sonu Kumar <sonu.27091999@gmail.com>
2022-05-27 09:57:19 +0530

    adding basic loader

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,39 +1,56 @@
 import React from 'react'
 import ListItem from "./ListItems/ListItem";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios'
+import Loader from '../UI/Loader';
 
 const Products = () => {
     const [items, setItems] = useState([]);
+    const [loader, setLoader] = useState(true);
 
-    //@@@@@@@@ using axioms
-    axios.get('https://hello-world-52dbb-default-rtdb.firebaseio.com/items.json')
-        .then(resopnse => {
-            const data = resopnse.data;
-            const transformedData = data.map((item, index) => {
-                return {
-                    ...item,
-                    id: index
-                }
-            })
-            console.log(transformedData);
-            setItems(transformedData);
-        })
-        .catch(error => console.log(error));
+
+    useEffect(() => {
+        async function fecthItem() {
+            try {
+                const response = await axios.get('https://hello-world-52dbb-default-rtdb.firebaseio.com/items.json');
+                let data = response.data;
+                const transformedData = data.map((item, index) => {
+                    return {
+                        ...item,
+                        id: index
+                    }
+                })
+                setLoader(false);
+                setItems(transformedData);
+            }
+            catch (error) {
+                setLoader(false);
+                console.log("errors : ", error);
+                alert('Some error occured');
+            }
+            finally {
+                setLoader(false);
+            }
+        }
+        fecthItem();
+    }, [])
 
     return (
-        <div className='product-list'>
-            <div className='product-list--wrapper'>
-                {/* <ListItem data={items[0]} />
+        <>
+            <div className='product-list'>
+                <div className='product-list--wrapper'>
+                    {/* <ListItem data={items[0]} />
                 <ListItem data={items[1]} /> */}
-                {/* {[<div>hello div</div>,<p>hello p</p>]} */}
-                {
-                    items.map((item) => {
-                        return <ListItem key={item.id} data={item} />
-                    })
-                }
+                    {/* {[<div>hello div</div>,<p>hello p</p>]} */}
+                    {
+                        items.map((item) => {
+                            return <ListItem key={item.id} data={item} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
+            {loader && <Loader />}
+        </>
     )
 }
 
