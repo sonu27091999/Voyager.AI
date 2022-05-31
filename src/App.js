8c696c659e0edcd1897adc3f0ca1385349be4547
Sonu Kumar <sonu.27091999@gmail.com>
2022-05-31 11:28:04 +0530

    Project Setup & Route addition

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Subheader from "./components/Layout/Subheader";
 import Products from "./components/Products/Products";
 import { Routes, Route } from 'react-router-dom'
+import AuthIndex from "./components/Auth";
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
       <Subheader />
       <Routes>
         {/* {We no need to write exact beause it is and update react-router-dom version 6.3.0} */}
-        <Route path="/404" element={<h1>404 Not found</h1>}></Route>
         <Route path="/" element={<Products />}>
           <Route path=":category" element={<Products />} />
         </Route>
+        {['login', 'signup'].map(path => <Route path={path} element={<AuthIndex path={path}/>} />)}
         <Route path="*" element={<h2>404 Not found</h2>}></Route>
       </Routes>
     </div>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import Cart from "../Cart/Cart"
 import SearchBox from "../UI/Search"
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
         <header>
             <div className="nav-brand">
-                <a href="" to="/">
+                <Link to="/">
                     <span>AmaKart</span>
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="30"
                         height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round"
@@ -16,12 +17,13 @@ const Header = () => {
                         <path d="M17 17h-11v-14h-2" />
                         <path d="M6 5l14 1l-1 7h-13" />
                     </svg>
-                </a>
+                </Link>
             </div>
             <div className="searchBox-container">
-                <SearchBox/>
+                <SearchBox />
             </div>
-            <Cart/>
+            <Link className="login-btn" to='/login'>Login</Link>
+            <Cart />
         </header >
     )
 }
