e25a2d0dfeb782bf2f7caaa6e51cb1a3c01df06b
Sonu Kumar <sonu.27091999@gmail.com>
2022-06-01 10:38:57 +0530

    Protected Routes & Redirection

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Products from "./components/Products/Products";
 import { Routes, Route } from 'react-router-dom'
 import AuthIndex from "./components/Auth";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { checkIsLoggedIn } from "./actions/auth";
 
 function App() {
-  const dispatch=useDispatch();
- 
+  const dispatch = useDispatch();
+  const authState = useSelector(state => state.auth);
   useEffect(() => {
-    dispatch(checkIsLoggedIn(()=>{}));
+    dispatch(checkIsLoggedIn(() => { }));
   }, [])
-  
+
 
   return (
     <div>
@@ -24,8 +24,20 @@ function App() {
         <Route path="/" element={<Products />}>
           <Route path=":category" element={<Products />} />
         </Route>
-        {['login', 'signup'].map((path,index) => <Route path={path} key={index} element={<AuthIndex path={path}/>} />)}
-        <Route path="*" element={<h2>404 Not found</h2>}></Route>
+        {
+          !authState.idToken &&
+          ['login', 'signup'].map((path, index) => <Route path={path} key={index} element={<AuthIndex path={path} />} />)
+        }
+        <Route to="/" from="/login" />
+        <Route to="/" from="/signup" />
+        <Route
+          path="/404"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </div>
   );
